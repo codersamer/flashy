1b5402f5409c0140fadffe25b11f2fb836e61c4c
Samer Moustafa <coder.sam@outlook.com>
2023-03-10 06:05:08 +0200

    Fix Views Registery

--- src/Resources/Views/renders/tailwind/message.blade.php
+++ src/Resources/Views/renders/tailwind/message.blade.php
@@ -1,29 +1,31 @@
-<div {!! $message->buildAttributes(['class' => ['alert', 'alert-'.$class, 'alert-dismissible fade show' ]]) !!}>
-    <div class="container">
+<div {!! $message->buildAttributes(['class' => ['alert', 'alert-' . $class, 'alert-dismissible fade show']]) !!}>
+    <div class="container-fluid">
         <div class="row align-items-center">
             <div class="col">
-                @if($message->hasTitle())
-                <h4 class="alert-heading">{{$message->getTitle()}}</h4>
+                @if ($message->hasTitle())
+                    <h4 class="alert-heading">{{ $message->getTitle() }}</h4>
                 @endif
-                @if($message->hasIcon())
-                <i class="fa {{ $message->getIcon() }} me-2"></i>
+                @if ($message->hasIcon())
+                    <i class="fa {{ $message->getIcon() }} me-2"></i>
                 @endif
                 {!! $message->getText() !!}
             </div>
         </div>
         @if ($message->hasButtons())
-        <div class="row mt-3 gx-2">
-            @foreach ($message->getButtons() as $button)
-            <div class="col-auto">
-                <a {!! $button->buildAttributes(['class' => 'btn btn-'.($button->getType()->value == 'outline' ? 'outline-' : '').$class]) !!}>
-                    @if($button->hasIcon())
-                    <i class="fa {{ $button->getIcon() }} me-2"></i>
-                    @endif
-                    {!! $button->getText() !!}
-                </a>
+            <div class="row mt-3 gx-2">
+                @foreach ($message->getButtons() as $button)
+                    <div class="col-auto">
+                        <a {!! $button->buildAttributes([
+                            'class' => 'btn btn-' . ($button->getType()->value == 'outline' ? 'outline-' : '') . $class,
+                        ]) !!}>
+                            @if ($button->hasIcon())
+                                <i class="fa {{ $button->getIcon() }} me-2"></i>
+                            @endif
+                            {!! $button->getText() !!}
+                        </a>
+                    </div>
+                @endforeach
             </div>
-            @endforeach
-        </div>
         @endif
     </div>
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
